5a98799d9db1099bcd80931bfd21c9003de93780
Yolanda <landa.collins14@gmail.com>
2019-01-28 20:18:33 +0000

    adding notes

---
+++ udemy/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+  {
+    title: "my next trip",
+    body: "I want to go to San Fran"
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eat a bit better"
+  },
+  {
+    title: "Office Modification",
+    body: "New Desk and Chair"
+  }
+];
